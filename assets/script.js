975a3a09e8d5cd4e16def5825cf26f7ce4cada49
pjrclarke <>
2023-05-11 15:02:53 +0000

    JS coding done and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,62 @@
 const cards = document.querySelectorAll('.easymodecard');
 
+    let userFlipped = false;
+    let lock = false;
+    let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    if(lock) return;
+    if (this === firstCard) return;
+
+
+    this.classList.add('flip');
+
+    if(!userFlipped) {
+        userFlipped = true;
+        firstCard=this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
 }
+
+function disableCards () {
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+
+    resetBoard();
+
+}
+
+function checkForMatch() {
+    let match = firstCard.dataset.framework === secondCard.dataset.framework 
+        match ? disableCards() : unflipCards();
+        return;
+}
+
+function unflipCards() {
+    lockboard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 800);
+}
+
+function resetBoard() {
+    [userFlipped, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+    });
+})();
+
 cards.forEach(card => card.addEventListener('click', flipCard));
